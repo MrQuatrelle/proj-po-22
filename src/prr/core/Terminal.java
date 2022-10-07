f78cf12b06e2832d4e86e23b5f32621574a5b924
MrQuatrelle <maigasmendo@gmail.com>
2022-10-07 10:15:46 +0100

    indenting everything with 4 spaces

--- src/prr/core/Terminal.java
+++ src/prr/core/Terminal.java
@@ -9,31 +9,31 @@ import java.io.Serializable;
  */
 abstract public class Terminal implements Serializable /* FIXME maybe addd more interfaces */{
 
-  /** Serial number for serialization. */
-  private static final long serialVersionUID = 202208091753L;
-  
-  // FIXME define attributes
-  // FIXME define contructor(s)
-  // FIXME define methods
-  
-  /**
-   * Checks if this terminal can end the current interactive communication.
-   *
-   * @return true if this terminal is busy (i.e., it has an active interactive communication) and
-   *          it was the originator of this communication.
-   **/
-  public boolean canEndCurrentCommunication() {
-    // FIXME add implementation code
-    return true;
-  }
-  
-  /**
-   * Checks if this terminal can start a new communication.
-   *
-   * @return true if this terminal is neither off neither busy, false otherwise.
-   **/
-  public boolean canStartCommunication() {
-    // FIXME add implementation code
-    return true;
-  }
+    /** Serial number for serialization. */
+    private static final long serialVersionUID = 202208091753L;
+
+    // FIXME define attributes
+    // FIXME define contructor(s)
+    // FIXME define methods
+
+    /**
+     * Checks if this terminal can end the current interactive communication.
+     *
+     * @return true if this terminal is busy (i.e., it has an active interactive communication) and
+     *          it was the originator of this communication.
+     **/
+    public boolean canEndCurrentCommunication() {
+        // FIXME add implementation code
+        return true;
+    }
+
+    /**
+     * Checks if this terminal can start a new communication.
+     *
+     * @return true if this terminal is neither off neither busy, false otherwise.
+     **/
+    public boolean canStartCommunication() {
+        // FIXME add implementation code
+        return true;
+    }
 }
